f494de7ebad03a38095e21d654211d365349e1e1
hasithaPerera2002 <hasithaperera2017@gmail.com>
2025-11-17 14:47:25 +0530

    change script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Hello, this script is running!"
+echo "Hello, this script is running!!!"
 
 echo "Git user.name: $(git config --global user.name)"
 echo "Git user.email: $(git config --global user.email)"
